b70c1dfec10b6ef0466abc66638bc2bf710c792a
argub <bugracicioglu@gmail.com>
2022-03-21 23:17:27 +0300

    boş div eklendi

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
         <Contacts/>
+        <div></div>
     </div>
 
   );
@@ -15,7 +16,3 @@ function App() {
 export default App;
 
 
-// Bak şimdi başından beri sorun ne biliyormusun biz git'de rapository oluştururken Readme olsun checkboxsını işaretledim
-// Sonra geldim bu aptal sitedeki add Readme.md komutunu yazdım bu sefer hep uzak sunucuda githubda hem sende github dosyası oluştu
-// ve bunlar çakıştı bunları birlieştrmen gerek böyle durumlarda bu:N(((((()))))) buna merge deniyor consoledan merge çözmeyi bilmiyorum yok heraldede
-// burda otomatik çözüyosun bal şimdi sağda
